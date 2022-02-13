a473025a88dc595f3964ece0c0a9ab1e73262767
Dennis Gillich <dennisgillich@live.de>
2022-02-13 16:39:05 +0100

    Übungen zu Fallunterscheidungen.

--- Program.cs
+++ Program.cs
@@ -4,16 +4,35 @@
 int age = 14;
 bool withParents = false;
 
-if (age >= 18)
+/* if (age >= 18)
+       {
+           Console.WriteLine("Du darfst den Film sehen!");
+       }
+       else if (withParents == true)
+       {
+           Console.WriteLine("Du darfst den Film dank deiner Eltern sehen!");
+       }
+       else
+       {
+           Console.WriteLine("Du darfst den Film nicht sehen!");
+       }
+       Console.ReadKey();
+*/
+
+
+int alter = 18;
+bool mobil = false;
+bool qualifiziert = true;
+bool testBestanden = false;
+bool schüchtern = false;
+
+if ((alter >= 18) && (mobil) && (qualifiziert || testBestanden) && !schüchtern)
 {
-    Console.WriteLine("Du darfst den Film sehen!");
-}
-else if (withParents == true)
-{
-    Console.WriteLine("Du darfst den Film dank deiner Eltern sehen!");
+    Console.WriteLine("Du bekommst den Job.");
 }
 else
 {
-    Console.WriteLine("Du darfst den Film nicht sehen!");
+    Console.WriteLine("Du bekommst den Job leider nicht.");
 }
+
 Console.ReadKey();
