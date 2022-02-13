2037812a88415345177210a2c16ac9ea6b5bc3ab
Dennis Gillich <dennisgillich@live.de>
2022-02-13 15:33:28 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int age = 14;
+bool withParents = false;
+
+if (age >= 18)
+{
+    Console.WriteLine("Du darfst den Film sehen!");
+}
+else if (withParents == true)
+{
+    Console.WriteLine("Du darfst den Film dank deiner Eltern sehen!");
+}
+else
+{
+    Console.WriteLine("Du darfst den Film nicht sehen!");
+}
+Console.ReadKey();
